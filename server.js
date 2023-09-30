af1736ca0e1d58e163a26834b4904a47f80f0496
EmmanuelLafenwa <16736949-EmmanuelLafenwa@users.noreply.replit.com>
2023-09-30 20:31:32 +0000

    Removed redundant files

---
+++ server.js
@@ -0,0 +1,87 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const auth = require('./auth.js');
+const routes = require('./routes.js');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+      res.render('profile', { username: req.user.username });
+    });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) {
+        return (null, false);
+      }
+      return done(null, user);
+    });
+  }));
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
